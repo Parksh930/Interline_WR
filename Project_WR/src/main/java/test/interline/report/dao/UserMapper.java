aff96bbd37780a7c139d0c2e9a180dc295ab0df9
_eugene_K <luv3mnm@gmail.com>
2020-04-16 16:17:31 +0900

    no message

--- Project_WR/src/main/java/test/interline/report/dao/UserMapper.java
+++ Project_WR/src/main/java/test/interline/report/dao/UserMapper.java
@@ -6,16 +6,12 @@ import test.interline.report.vo.reportListVO;
 
 public interface UserMapper {
 
-<<<<<<< HEAD
 	
 ArrayList<reportListVO> getMy_List();
-=======
 
-	ArrayList<reportListVO> getMy_List();
 
 	boolean writeReportList(reportListVO report);
 
->>>>>>> f11f4e804d635fe52a1d3e58bc2301e729a5ae1f
 	//public int writeReport(ReportMain report);
 
 }
