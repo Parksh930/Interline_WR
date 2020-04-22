14d61e5fdc1fc7c9bdef008fee0cdb5155f109dd
ohsaeam <saeam.oh@gmail.com>
2020-04-22 12:05:32 +0900

    no message

--- Project_WR/src/main/java/test/interline/report/AdminController.java
+++ Project_WR/src/main/java/test/interline/report/AdminController.java
@@ -26,13 +26,13 @@ import test.interline.report.vo.userVO;
 public class AdminController {
 
 	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
-	
+
 	private static final int countPerPage=10;		
 	private static final int pagePerGroup=10;	
-	
+
 	@Autowired
 	AdminDAO dao;
-	
+
 	//�꺃�깮�꺖�깉�굮沃���
 
 	@RequestMapping(value = "/admin/readReport", method = RequestMethod.GET)
@@ -40,7 +40,7 @@ public class AdminController {
 
 		logger.debug("reportNum:{}", reportNum);
 		model.addAttribute("ReportNumValue", reportNum); // 모델값
-		
+
 		HttpSession session = request.getSession();
 		String getMobilecheck=(String)session.getAttribute("mobileCheck");
 		if(getMobilecheck.equals("")){
@@ -51,32 +51,32 @@ public class AdminController {
 			System.out.println("관리자모바일접속");
 			return "Admin/readReportMobile";
 		}
-		
+
 		return "Admin/readReport";
 	}
-	
+
 	//�꺃�깮�꺖�깉�꺁�궧�깉
 	@RequestMapping(value = "/admin/reportList", method = RequestMethod.GET)
 	public String getList2(Model model,@RequestParam(value="page", defaultValue="1") int page) {
 		logger.debug("pageNum:{}", page);
-		
+
 		int total = dao.getTotal();
 		PageNavigator navi = new PageNavigator(countPerPage, pagePerGroup, page, total);
-		
+
 		ArrayList<reportListVO> all_list = dao.getAll_List(navi.getStartRecord(), navi.getCountPerPage());
-		
+
 		model.addAttribute("navi", navi);
 		model.addAttribute("report_all",all_list);
 		return "Admin/reportList";
 	}
-	
+
 	@RequestMapping(value="/admin/registerUser", method=RequestMethod.GET)
 	public String insertUserForm() {
 		logger.debug("RegisterUser");
-		
+
 		return "Admin/registerUser";
 	}
-	
+
 
 	@ResponseBody
 	@RequestMapping(value="/admin/check_multiple", method=RequestMethod.POST)
@@ -85,38 +85,38 @@ public class AdminController {
 		userVO check_result = null;
 		check_result = dao.check_Multiple("check_num",userNum);
 		check_result = dao.check_Multiple("check_id",userId);
-		
+
 		return "true";
 	}
-	
+
 	@RequestMapping(value="/admin/registerUser", method=RequestMethod.POST)
 	public String insertUser(userVO user) {
 		logger.debug("insert_user:{}",user);
-		
-		 int result = dao.insertUser(user); 
-		 
-		 if(result == 1) {
-			 logger.debug("登録成功");
-		 }else {
-			 logger.debug("登録失敗");
-		 }
-		
+
+		int result = dao.insertUser(user); 
+
+		if(result == 1) {
+			logger.debug("登録成功");
+		}else {
+			logger.debug("登録失敗");
+		}
+
 		return "redirect:/mainMenu";
 	}
-	
-/*	@RequestMapping(value="/admin/printAll", method=RequestMethod.GET)
+
+	/*	@RequestMapping(value="/admin/printAll", method=RequestMethod.GET)
 	public String printAll(HttpServletRequest request) {
-		
-		
-		 * HttpSession session = request.getSession(); String
-		 * getMobilecheck=(String)session.getAttribute("mobileCheck");
-		 * if(getMobilecheck.equals("")){ getMobilecheck="1"; }
-		 * System.out.println("관리자모바일: "+getMobilecheck); if(getMobilecheck.equals("1"))
-		 * { System.out.println("관리자모바일접속"); return "Admin/AllReportMobile"; }
-		 
-		
+
+
+	 * HttpSession session = request.getSession(); String
+	 * getMobilecheck=(String)session.getAttribute("mobileCheck");
+	 * if(getMobilecheck.equals("")){ getMobilecheck="1"; }
+	 * System.out.println("관리자모바일: "+getMobilecheck); if(getMobilecheck.equals("1"))
+	 * { System.out.println("관리자모바일접속"); return "Admin/AllReportMobile"; }
+
+
 		return "Admin/AllReport";*/
-	}
-	
-	
 }
+
+
+
